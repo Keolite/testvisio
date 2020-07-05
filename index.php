f1c2b1600e2f3bafa086b60ec86c40ab2e1330f0
Keolite <e.baillysalins@outlook.fr>
2020-07-05 15:29:25 +0000

    Suite du parcours

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); ?>
+<?php 
+
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+$nom = 'Martin';
+$prenom =  'Julie';
+
+$template = $twig->load('base.html.twig');
+echo $template->render(['nom' => $nom, 'surname' => $prenom]);
+
+
